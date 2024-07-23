6206c9739aad0e39d6c58969a8ac84fcd38b6310
Nicolas Cañizares <nicolasjcg523@gmail.com>
2024-07-23 16:45:47 -0500

    primer_commit

---
+++ ejercicio_a_enviar_1.py
@@ -0,0 +1,8 @@
+nombre = input("Digite su nombre: ")
+edad = int(input("Digite su edad: "))
+
+print("\n\tBienvenido " + nombre + " con edad " + str(edad) + " years.")
+
+numero = input("Digite un numero: ")
+numero = int(numero)
+print(numero * 2)
